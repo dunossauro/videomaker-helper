eb294e1df0a78fe9b9e1e8f2cf369ea61a78036f
dunossauro <mendesxeduardo@gmail.com>
2023-10-14 03:39:03 -0300

    FIX: Forcing the configuration files creation

--- vmh/settings.py
+++ vmh/settings.py
@@ -4,5 +4,16 @@ from appdirs import user_data_dir
 
 user_path = Path(user_data_dir('vmh', 'vmh'))
 cache_db_path = user_path / 'vmh_cache.json'
+eq_config_path = user_path / 'config.json'
+
+# Create base files
+if not user_path.exists():
+    user_path.mkdir(parents=True)
+
+if not cache_db_path.exists():
+    cache_db_path.touch()
+
+if not eq_config_path.exists():
+    eq_config_path.touch()
 
 __version__ = '0.1.0'
